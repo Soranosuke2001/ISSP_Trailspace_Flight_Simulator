971498c5c4ddc0be8ce9d4848056858b7a94b1f3
Caner Boz <ihsancanerboz@gmail.com>
2024-02-11 00:01:10 +0100

    add controller step to main and clean other modules

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from repo.model import JsbsimInterface
+from controller import Controller
 
 from helpers.read_config import read_setup, read_ic_aircraft
 
 if __name__ == "__main__":
     aircraft_model, timestep, mass, throttle, wind, ini_vel, ini_alt = read_setup()
-    phiItgt, thetaItgt, psiItgt, altItgt = read_ic_aircraft()
 
     # set the initial conditions
     sim = JsbsimInterface(
-        aircraft_model, 
-        ini_alt, 
-        ini_vel, 
-        mass, 
+        aircraft_model,
+        ini_alt,
+        ini_vel,
+        mass,
         wind,
         throttle,
     )
@@ -21,15 +21,22 @@ if __name__ == "__main__":
     sim.check_ic()
     sim.print_config()
 
+    # initialize controller object
+    controllerObj = Controller()
+
     # simulation
     while True:
+
+        # call the controller step function
+        sim_inputs = {
+            'phiSetpoint': 0.15,
+            'phi': sim.exec['attitude/roll-rad'],
+        }
+        ctrlOutput = controllerObj.step(sim_inputs)
+
+        # run simulation one time step
+        sim.set_throttle(ctrlOutput['throttle'])
+        sim.set_control_surfaces(ctrlOutput['aileronComm'], 0.0, 0.0)
         sim.run()
 
-        # set the throttle
-        sim.set_throttle()
-
-        # setting aileron
-        aileron = sim.calc_aileron(phiItgt)
-
         # continue from here
-    
